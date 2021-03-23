176f263a233a999d01ec363fc37d455860565690
Jaehyeon Lim <xhwogusxh@gmail.com>
2021-03-23 22:56:13 +0900

    beforeDB

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,11 @@
 import path from "path";
+import { db } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
   //   let p = path.join("..", "views", "home.pug");
   //   res.render(p);
-  res.render("home", { pageTitle: "Home" });
+  res.render("home", { pageTitle: "Home", videos: db });
 };
 
 export const search = (req, res) => {
@@ -11,19 +13,27 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos: db });
 };
 
 export const videos = (req, res) => {
   res.send("videos");
 };
 
-export const upload = (req, res) => {
+export const getUpload = (req, res) => {
   res.render("upload");
 };
 
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description },
+  } = req;
+  //To Do : Upload and save video
+  res.redirect(routes.videoDetail(11));
+};
+
 export const videoDetail = (req, res) => {
-  res.send("video Detail");
+  res.render("videoDetail");
 };
 
 export const editVideo = (req, res) => {
